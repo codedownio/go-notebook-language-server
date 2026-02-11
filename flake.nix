e9bd43d705c6d0ee315b7f7d3a5aa65b52ac16b7
Tom McLaughlin <tom@codedown.io>
2026-02-10 18:52:33 -0800

    Able to build grand combined github artifacts

--- flake.nix
+++ flake.nix
@@ -26,6 +26,9 @@
 
         flake = (pkgs.hixProject compiler-nix-name).flake {};
         flakeStatic = (pkgs.pkgsCross.musl64.hixProject compiler-nix-name).flake {};
+        flakeDarwin = (pkgs.pkgsCross.aarch64-darwin.hixProject compiler-nix-name).flake {};
+        flakeAarch64Linux = (pkgs.pkgsCross.aarch64-multiplatform.hixProject compiler-nix-name).flake {};
+        flakeStaticAarch64Linux = (pkgs.pkgsCross.aarch64-multiplatform-musl.hixProject compiler-nix-name).flake {};
 
         # Build go-parser from source (stdlib only, no vendorHash needed)
         go-parser = pkgs.buildGoModule {
@@ -47,6 +50,15 @@
           mkdir -p $out/bin
           makeWrapper ${gnls}/bin/go-notebook-language-server $out/bin/go-notebook-language-server \
             --prefix PATH : ${go-parser}/bin
+        '';
+
+        packageForGitHub' = systemToUse: cnls: pkgs.runCommand "go-notebook-language-server-${cnls.version}-${systemToUse}" {} ''
+          name="go-notebook-language-server-${cnls.version}-${systemToUse}"
+
+          mkdir -p to_zip
+          cp -r ${cnls}/* to_zip
+          mkdir -p $out
+          tar -czvf $out/$name.tar.gz -C to_zip .
         '';
 
       in
@@ -77,12 +89,36 @@
 
             static = flakeStatic.packages."go-notebook-language-server:exe:go-notebook-language-server";
             dynamic = flake.packages."go-notebook-language-server:exe:go-notebook-language-server";
+            darwin = flakeDarwin.packages."go-notebook-language-server:exe:go-notebook-language-server";
+            aarch64Linux = let
+              executable = flakeAarch64Linux.packages."go-notebook-language-server:exe:go-notebook-language-server";
+              libs = pkgs.callPackage ./nix/dynamic-aarch64-closure.nix {
+                inherit executable;
+                executableName = "go-notebook-language-server";
+                pkgsCross = pkgs.pkgsCross.aarch64-multiplatform;
+              };
+            in pkgs.runCommand "go-notebook-language-server-aarch64-dynamic" { passthru = { inherit (executable) version; }; } ''
+                 mkdir -p $out/bin
+                 cp -r ${executable}/bin/* $out/bin
+                 cp -r ${libs}/lib/* $out/bin
+               '';
+            staticAarch64Linux = flakeStaticAarch64Linux.packages."go-notebook-language-server:exe:go-notebook-language-server";
 
             # Print a trivial PATH that we can use to run kernel and LSP tests, to ensure
             # they aren't depending on anything on the test machine's PATH.
             print-basic-path = pkgs.writeShellScriptBin "basic-path.sh" ''
               echo ${pkgs.lib.makeBinPath (with pkgs; [coreutils go go-parser])}
             '';
+
+            grandCombinedGithubArtifacts = pkgs.symlinkJoin {
+              name = "go-notebook-language-server-grand-combined-artifacts";
+              paths = [
+                (packageForGitHub' "x86_64-linux" self.packages.x86_64-linux.static)
+                (packageForGitHub' "aarch64-linux" aarch64Linux)
+                (packageForGitHub' "x86_64-darwin" self.packages.x86_64-darwin.dynamic)
+                (packageForGitHub' "aarch64-darwin" self.packages.aarch64-darwin.dynamic)
+              ];
+            };
 
             nixpkgsPath = pkgs.writeShellScriptBin "nixpkgsPath.sh" "echo -n ${pkgs.path}";
           });
