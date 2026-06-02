2cb3a5062ef09c70cc7f04a425ee2f120d202d96
Tom McLaughlin <tom@codedown.io>
2026-06-01 17:37:45 -0700

    Fix linking on darwin the way cnls does it

--- nix/macos-modules.nix
+++ nix/macos-modules.nix
@@ -1,24 +1,25 @@
 { pkgs }:
 
 {
-  packages.cpp-notebook-language-server.components.exes.cpp-notebook-language-server.postInstall = ''
+  packages.go-notebook-language-server.components.exes.go-notebook-language-server.postInstall = ''
     ${builtins.readFile ./fix-dylib.sh}
 
-    fix_dylib "$out/bin/cpp-notebook-language-server" libiconv.2.dylib libiconv.dylib
-    fix_dylib "$out/bin/cpp-notebook-language-server" libffi.8.dylib libffi.dylib
-    fix_dylib "$out/bin/cpp-notebook-language-server" libpcre.1.dylib libpcre.dylib
-    fix_dylib "$out/bin/cpp-notebook-language-server" libc++.1.0.dylib libc++.dylib
-    fix_dylib "$out/bin/cpp-notebook-language-server" libc++abi.1.0.dylib libc++abi.dylib
-    check_no_nix_refs "$out/bin/cpp-notebook-language-server"
+    fix_dylib "$out/bin/go-notebook-language-server" libiconv.2.dylib libiconv.dylib
+    fix_dylib "$out/bin/go-notebook-language-server" libpcre.1.dylib libpcre.dylib
+    check_no_nix_refs "$out/bin/go-notebook-language-server"
 
-    strip "$out/bin/cpp-notebook-language-server"
+    strip "$out/bin/go-notebook-language-server"
   '';
 
-  packages.cpp-notebook-language-server.components.exes.cpp-notebook-language-server.configureFlags = let
-    # Nixpkgs can't currently give us a cross-compiled x86_64-darwin libffi.a when we're building on aarch64-darwin.
-    # So, we bundle one in the repo.
-    # Tried to also detect if we're on aarch64-darwin, so it can work normally if the build machine is x86_64-darwin,
-    # but that is deliberately difficult here (builtins.currentSystem is considered an "impure builtin".)
+  packages.go-notebook-language-server.components.exes.go-notebook-language-server.configureFlags = let
+    # Statically link libffi so the binary doesn't carry a dynamic dependency on a
+    # /nix/store libffi.7.dylib that won't exist on an end user's machine. (The
+    # system /usr/lib/libffi.dylib is too old to repoint to reliably.)
+    #
+    # Nixpkgs can't currently give us a cross-compiled x86_64-darwin libffi.a when
+    # we're building on aarch64-darwin, so we bundle one in the repo for that target.
+    # Detecting the build machine's arch here is deliberately difficult
+    # (builtins.currentSystem is an "impure builtin"), so we key off the target.
     libffi = if pkgs.stdenv.targetPlatform.system == "x86_64-darwin"
              then "${../assets/libffi.a}"
              else "${pkgs.pkgsStatic.libffi}/lib/libffi.a";
--- nix/overlays/hix-project.nix
+++ nix/overlays/hix-project.nix
@@ -16,6 +16,10 @@ final: prev: {
         packages.file-io.components.library.configureFlags = [''-f os-string''];
 
         packages.go-notebook-language-server.components.exes.go-notebook-language-server.dontStrip = false;
-      }];
+      }]
+      # On macOS the dynamic build links libpcre/libiconv/libffi from /nix/store
+      # paths that won't exist on an end user's machine, so dyld refuses to start
+      # the binary. Repoint those at the system libraries / static-link libffi.
+      ++ final.lib.optional final.stdenv.hostPlatform.isDarwin (import ../macos-modules.nix { pkgs = final; });
     };
 }
